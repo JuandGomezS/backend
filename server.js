40f2353e73c832b8b798130eec9ea0401187743c
Juan_GomezS <75592506+LYUJuanete@users.noreply.github.com>
2021-09-09 18:34:37 -0500

    Featured: Router express challenge

--- server.js
+++ server.js
@@ -1,42 +1,74 @@
 import express from 'express';
 
-var array =[];
+
 
 const app = express();
 const PORT = 8080;
+const productsRouter = express.Router();
+
+let productos =[];
+let newId= 0;
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api', productsRouter);
+
+app.use(express.static('front'));
 
 const server = app.listen(PORT, ()=>{
     console.log('Servidor HTTP escuchando en el puerto', server.address().port);
 });
 server.on('error', error=>console.log('Error en servidor', error));
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 
 
-app.get('/api/productos/listar', (req,res)=>{
+
+productsRouter.get('/productos/listar', (req,res)=>{
     const object={error : 'no hay productos cargados'};
-    res.json(array.length>0? array: object)
+    res.json(productos.length>0? {productos, response: '200 OK'}: {object, response: '400 Bad request'})
 });
 
-app.get('/api/productos/listar/:id', (req,res)=>{
+productsRouter.get('/productos/listar/:id', (req,res)=>{
     let params = req.params;
     let id = params.id;
-    const product=array.find(elemento=> elemento.id == id)
+    const product=productos.find(elemento=> elemento.id == id)
     const object={error : 'producto no encontrado'};
-    res.json(product? product: object);   
+    res.json(product? {product, response: '200 OK'}: {object, response: '400 Bad request'});   
 });
 
-app.post('/api/productos/guardar', (req,res)=>{
+productsRouter.post('/productos/guardar', (req,res)=>{
     let body = req.body;
+    productos.length>0?newId= productos[productos.length - 1].id +1:newId=1;
     let object= {
-        id: array.length + 1,
+        id: newId,
         title: body.title,
         price: body.price,
         thumbnail: body.thumbnail
     };
-    array.push(object);
-    res.json(object);
+    productos.push(object);
+    res.json({body, response: '200 OK'});
+});
+
+productsRouter.put('/productos/actualizar/:id', (req,res)=>{
+    let params = req.params;
+    let id = params.id;
+    let index = productos.findIndex(x => x.id ==id);
+    const succes={response: 'Producto actualizado'};
+    const object={error : 'producto no encontrado'};
+    res.json(index>=0? {succes, response: '200 OK'}: {object, response: '400 Bad request'});
+});
+
+productsRouter.delete('/productos/borrar/:id', (req,res)=>{
+    let params = req.params;
+    let id = params.id;
+    let index = productos.findIndex(x => x.id ==id);
+    productos.splice(index,1)
+    const succes={response: 'Producto eliminado'}
+    const object={error : 'producto no encontrado'};
+    res.json(index>=0? {succes, response: '200 OK'}: {object, response: '400 Bad request'});
 });
 
     
